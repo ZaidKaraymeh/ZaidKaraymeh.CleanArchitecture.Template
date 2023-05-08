dfe32e4f6ceb62ae8cb56538a4f69ed3f6c39918
Zaid <karaymehzaid@gmail.com>
2023-05-08 23:11:02 +0300

    movie model

---
+++ ZaidKaraymeh.CleanArchitecture.Core/Commands/Movie/CreateMovie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZaidKaraymeh.CleanArchitecture.Core.Commands.Movie
+{
+    internal class CreateMovie
+    {
+    }
+}
---
+++ ZaidKaraymeh.CleanArchitecture.SharedKernal/Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZaidKaraymeh.CleanArchitecture.SharedKernal.Models
+{
+    internal class Movie
+    {
+    }
+}
